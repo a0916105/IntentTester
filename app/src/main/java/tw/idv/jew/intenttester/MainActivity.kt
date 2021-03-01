2521338115c209e7b239ec147e2b5042226c5ab3
a0916105 <whdofjew@gmail.com>
2021-03-01 20:03:25 +0800

    Broadcast動態註冊

--- app/src/main/java/tw/idv/jew/intenttester/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/intenttester/MainActivity.kt
@@ -1,11 +1,24 @@
 package tw.idv.jew.intenttester
 
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    //28-3_Broadcast動態註冊
+    val myReceiver = MyReceiver()
+    override fun onStart() {
+        super.onStart()
+        IntentFilter().apply {
+            addAction("tw.idv.jew.CHAT_INCOMING")
+        }.also {
+            registerReceiver(myReceiver, it)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,10 +35,18 @@ class MainActivity : AppCompatActivity() {
         /*val dial = Intent(Intent.ACTION_DIAL)
         startActivity(dial)*/
         //28-3_Broadcast
-        Intent().apply {
-            setAction("tw.idv.jew.CHAT_INCOMING")
-        }.also {
-            sendBroadcast(it)
+        var button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Intent().apply {
+                setAction("tw.idv.jew.CHAT_INCOMING")
+            }.also {
+                sendBroadcast(it)
+            }
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(myReceiver)
     }
 }
